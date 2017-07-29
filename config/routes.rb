659296a75e343dfd3299932b740fba14a168eeb5
Juan David Aristizábal <jd.ac@outlook.com>
2017-07-28 20:40:49 -0500

    add filter for posts by tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
   resources :posts
+  get 'tag/:name' => 'tags#filter', as :tag_filter
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
